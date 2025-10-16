5a93e03756459825c7c7e325cb4ccb212d7affba
Felipe Rivetti <fporto@sarrat.com.br>
2025-10-16 20:51:55 -0300

    refactor: remove unused select_university import

--- src/components/__init__.py
+++ src/components/__init__.py
@@ -1,4 +1,3 @@
 from .sidebar import render_sidebar
 from .header import render_header
 from .spreadsheet_uploader import render_spreadsheet_uploader
-from .select_university import select_university
